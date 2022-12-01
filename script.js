d2f67ea2035b7c1e629a00ff42c2d963c6e3a92c
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-12-01 11:32:18 -0300

    update in themes of page

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 //   link.addEventListener('click', ativo)
 // })
 
-/* aumentar o texto */
+// aumentar o texto
 function initTextoMaior() {
   const textos = document.querySelectorAll(".texto");
 
@@ -34,6 +34,21 @@ initTextoMaior();
 function initTemas() {
   const claro = document.querySelector("body").classList;
   const temas = document.querySelectorAll(".tema");
+
+  const temaPC = window.matchMedia("(prefers-color-scheme: dark)");
+  if (temaPC.matches) {
+    document.body.classList.remove("claro");
+    document.body.classList.add("escuro");
+    temas.forEach((tema) => {
+      tema.classList.add("ativo");
+    });
+  } else {
+    document.body.classList.remove("escuro");
+    document.body.classList.add("claro");
+    temas.forEach((tema) => {
+      tema.classList.remove("ativo");
+    });
+  }
 
   function alteraTema() {
     if (claro[0] === "claro") {
