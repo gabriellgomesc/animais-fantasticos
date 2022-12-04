e6a8f1d405111c3b283dff077d5f42199dace6f4
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-12-04 07:54:48 -0300

    some corrections in menu-mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -5,10 +5,12 @@ export default function initMenuMobile() {
   function openMenu() {
     if (menuList.classList.contains("desativo")) {
       menuList.classList.remove("desativo");
+      menuList.classList.remove("desativo2");
       menuList.classList.add("ativo");
       menuButton.classList.add("ativo");
     } else {
       menuList.classList.add("desativo");
+      menuList.classList.add("desativo2");
       menuList.classList.remove("ativo");
       menuButton.classList.remove("ativo");
     }
