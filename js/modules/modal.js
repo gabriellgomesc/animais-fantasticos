dc8e4518a064165fb0ce57afb7b2c2d3c65d07dd
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-12-04 14:46:58 -0300

    corrections menu-mobile 4

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -2,14 +2,17 @@ export default function initModal() {
   const botaoLogin = document.querySelector('[data-modal="abrir"]');
   const botaoClose = document.querySelector('[data-modal="fechar"]');
   const containerModal = document.querySelector('[data-modal="container"]');
-  const menuList = document.querySelector('[data-menu="list"');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const menuButton = document.querySelector('[data-menu="button"]');
 
   if (botaoLogin && botaoClose && containerModal) {
     function toggleModal(event) {
       event.preventDefault();
       containerModal.classList.toggle("ativo");
       menuList.classList.remove("ativo");
+      menuButton.classList.remove("ativo");
       menuList.classList.add("desativo");
+      menuButton.classList.add("desativo");
     }
 
     function outsideClick(event) {
